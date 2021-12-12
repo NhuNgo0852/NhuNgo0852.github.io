04bf2809d2e67dd0475b6afce00a2708521ac38c
NhuNgo0852 <nhungo0852@gmail.com>
2021-12-12 18:41:16 +0100

    delete sale off banner

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelectorS(".menu_icon");
-const navBar = document.querySelector(".navbar");
-
-menuIcon.addEventListener("click", function (){
-    navBar.classList.toggle("active");    
-})
