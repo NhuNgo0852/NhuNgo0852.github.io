d4ba99a97f90a3490d91ad6054cbafa919ab824c
NhuNgo0852 <nhungo0852@gmail.com>
2022-03-30 00:30:20 +0200

    change font-size, margin & padding

--- navBar.js
+++ navBar.js
@@ -6,17 +6,17 @@ const closeIcon = document.getElementById("close_icon");
 
 // The addEventListener() method attaches an event handler to an element.
 menuIcon.addEventListener("click", openMenu); 
+closeIcon.addEventListener("click",closeMenu);
 
 function openMenu() {
     navBar.style.display = "flex";
     menuIcon.style.display = "none";
-    closeIcon.addEventListener("click",closeMenu);
-    function closeMenu() {
-        navBar.style.display = "none";
-        menuIcon.style.display = "block";
-    }
 }
 
+function closeMenu() {
+    navBar.style.display = "none";
+    menuIcon.style.display = "flex";
+}
 //Add active class to the current button (highlight it)
 
 
